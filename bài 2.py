66aae3000828bf4595c3306a0e91a627f58370c2
20231960-beep <20231960@eaut.edu.vn>
2026-03-31 08:24:44 +0700

    Delete bài 2.py

--- bài 2.py
+++
@@ -1,8 +0,0 @@
-a = int(input("Nhập a: "))
-b = int(input("Nhập b: "))
-c = int(input("Nhập c: "))
-
-if a + b > c and a + c > b and b + c > a:
-    print("Độ dài ba cạnh tam giác")
-else:
-    print("Đây không phải độ dài ba cạnh tam giác")
